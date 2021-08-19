f142ccf692e12f9c7ed368755a7e9a25f818609b
AR Shahin <mdshahinmije96@gmail.com>
2021-08-19 22:27:25 +0600

    doc intro code

--- resources/views/includes/introduction.blade.php
+++ resources/views/includes/introduction.blade.php
@@ -1,5 +1,5 @@
 <div id="introduction" class="mt-2">
     <h3>#Introduction</h3>
-   <p class="lead"> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Neque excepturi mollitia error voluptas sint similique, assumenda aliquid quod eius! Necessitatibus eveniet ipsa optio consequatur velit, nostrum officiis veniam eum quae eos eius quam, omnis dolor temporibus dolorem aspernatur voluptate, dicta tempora natus ratione suscipit facere illum ad! Similique, sint autem?
-    voluptas sint similique, assumenda aliquid quod eius! Necessitatibus eveniet ipsa optio consequatur velit, nostrum officiis veniam eum quae eos eius quam, omnis dolor temporibus dolorem aspernatur voluptate, dicta tempora natus ratione suscipit facere illum ad! Similique, sint autem?</p>
+   <p class="lead"> Welcome to <b class="text-success">talkToAPI</b>. TalktoAPI is a opensource API provider like Json Placeholder API provider. TalktoAPI provides you several kinds of API,such as CRUD, Blog and Ecommerce. The main goal of TalktoAPI is
+</p>
 </div>
