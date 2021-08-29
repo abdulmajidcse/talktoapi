c328d97ff02920a8fe77546d7223712d271fdc29
AR Shahin <mdshahinmije96@gmail.com>
2021-08-29 09:44:48 +0600

    docs modify

---
+++ resources/views/docs/authentication.blade.php
@@ -0,0 +1,20 @@
+@extends('layout.app')
+@section('title' ,'Authentication')
+@section('app_content')
+    <div class="Home">
+        <h1>#Authentication</h1>
+        <ul>
+            <li>
+                <h6><a href="#introduction">Introduction</a></h6>
+            </li>
+            <li>
+                <h6><a href="#howToUse">How to use?</a></h6>
+            </li>
+            <li>
+                <h6><a href="#versionAndDevelopers">Version and Developers</a></h6>
+            </li>
+        </ul>
+        <hr>
+
+    </div>
+@stop
