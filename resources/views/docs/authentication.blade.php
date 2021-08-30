4b6c2457ab448420c8096a4371342fba20e8436a
AR Shahin <mdshahinmije96@gmail.com>
2021-08-30 12:03:14 +0600

    auth user profile and logout docs

--- resources/views/docs/authentication.blade.php
+++ resources/views/docs/authentication.blade.php
@@ -14,6 +14,9 @@
                 <h6><a href="#Login">Login</a></h6>
             </li>
             <li>
+                <h6><a href="#MyProfile">My Profile</a></h6>
+            </li>
+            <li>
                 <h6><a href="#Logout">Logout</a></h6>
             </li>
         </ul>
@@ -23,7 +26,7 @@
             <div class="container">
                 <div id="introduction">
                     <h5 class="text-info">Authentication</h5>
-                    <p>In this API we are going to see <b>Authentication</b>. To access authenticated API first, you have to create an account. After creating an account then you have to login. The login method returns a access token which you will use whenever send a request for authenticating API. You have to send this access token by query parameter.</p>
+                    <p>In this API we are going to see <b>Authentication</b>. To access authenticated API first, you have to create an account. After creating an account then you have to login. The login method returns an access token which you will use whenever send a request for authenticating API. You have to send this access token by query parameter.</p>
                 </div>
                 <h4>API Request:</h4>
                 <hr>
@@ -31,7 +34,7 @@
                 <x-docs.header/>
                 <!-- / Header -->
 
-                <!-- Login -->
+                <!-- Register -->
                 <x-docs.section id="Register">
                     <x-slot name="number">1</x-slot>
                     <x-slot name="url">{{ url('/api/register') }}</x-slot>
@@ -63,6 +66,107 @@
 }
                     </x-slot>
                 </x-docs.section>
+
+<hr>
+                      <!-- Login -->
+                      <x-docs.section id="Login">
+                        <x-slot name="number">2</x-slot>
+                        <x-slot name="url">{{ url('/api/login') }}</x-slot>
+                        {{-- <x-slot name="title">title</x-slot> --}}
+                        <x-slot name="request">
+var formdata = new FormData();
+    formdata.append("email", "ars@mail.com");
+    formdata.append("password", "12345678");
+
+    var requestOptions = {
+    method: 'POST',
+    body: formdata,
+    redirect: 'follow'
+    };
+
+    var requestOptions = {
+    method: 'POST',
+    body: formdata,
+    redirect: 'follow'
+    };
+
+fetch("{{ url('http://127.0.0.1:8000/api/login') }}", requestOptions)
+    .then(response => response.text())
+    .then(result => console.log(result))
+    .catch(error => console.log('error', error));
+                        </x-slot>
+                        <x-slot name="method">POST</x-slot>
+                        <x-slot name="response">
+    {
+        "success": true,
+        "message": "Access Token generated successfully!",
+        "data": {
+            "access_token": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC8xMjcuMC4wLjE6ODAwMFwvYXBpXC9sb2dpbiIsImlhdCI6MTYzMDMwMjI5MSwiZXhwIjoxNjMwMzA1ODkxLCJuYmYiOjE2MzAzMDIyOTEsImp0aSI6IlU5MUtnYmpPOFh1VWd4VWkiLCJzdWIiOjMsInBydiI6IjIzYmQ1Yzg5NDlmNjAwYWRiMzllNzAxYzQwMDg3MmRiN2E1OTc2ZjcifQ.D0RkG64WfpOPJxq1FqypMVNeB1SR6ceVz7bZ69kxCl8",
+            "token_type": "bearer",
+            "expires_in": 3600
+        }
+    }
+                        </x-slot>
+                    </x-docs.section>
+                    <hr>
+                     <!-- MyProfile -->
+                     <x-docs.section id="MyProfile">
+                        <x-slot name="number">3</x-slot>
+                        <x-slot name="url">{{ url('/api/user?token={access_token}') }}</x-slot>
+                        {{-- <x-slot name="title">title</x-slot> --}}
+                        <x-slot name="request">
+var requestOptions = {
+    method: 'GET',
+    redirect: 'follow'
+    };
+
+    fetch("{{ url('http://127.0.0.1:8000/api/user?token={access_token}') }}", requestOptions)
+        .then(response => response.text())
+        .then(result => console.log(result))
+        .catch(error => console.log('error', error));
+                        </x-slot>
+                        <x-slot name="method">GET</x-slot>
+                        <x-slot name="response">
+{
+    "success": true,
+    "message": "Data Retrieved Successfully!",
+    "data": {
+        "id": 3,
+        "name": "AR Shahin",
+        "email": "ars@mail.com",
+        "created_at": "2021-08-30 05:08:33 AM",
+        "updated_at": "2021-08-30 05:08:33 AM"
+    }
+}
+                        </x-slot>
+                    </x-docs.section>
+
+<hr>
+                    <!-- Logout -->
+                    <x-docs.section id="Logout">
+                        <x-slot name="number">4</x-slot>
+                        <x-slot name="url">{{ url('http://127.0.0.1:8000/api/logout?token={access_token}') }}</x-slot>
+                        {{-- <x-slot name="title">title</x-slot> --}}
+                        <x-slot name="request">
+var requestOptions = {
+    method: 'POST',
+    redirect: 'follow'
+    };
+
+    fetch("{{ url('http://127.0.0.1:8000/api/logout?token={access_token}') }}", requestOptions)
+        .then(response => response.text())
+        .then(result => console.log(result))
+        .catch(error => console.log('error', error));
+                        </x-slot>
+                        <x-slot name="method">POST</x-slot>
+                        <x-slot name="response">
+{
+    "success": true,
+    "message": "Successfully logged out!",
+    "data": []
+}
+                        </x-slot>
+                    </x-docs.section>
             </div>
         </section>
     </div>
