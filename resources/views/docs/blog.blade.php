59b5455fb536b5800129668e52f4f46326c4119f
AR Shahin <mdshahinmije96@gmail.com>
2021-09-03 10:47:54 +0600

    docs post api

--- resources/views/docs/blog.blade.php
+++ resources/views/docs/blog.blade.php
@@ -19,11 +19,21 @@
                 </ul>
             </li>
             <li>
-                <h6><a href="#blogPost">Post(Blog)</a></h6>
+                <h6><a href="#postIntroduction">Post(Blog)</a></h6>
+                <ul>
+                    <li><a href="#getPosts">GET <small>(Fetch all Post)</small></a></li>
+                    <li><a href="#postCreate">POST <small>(Create a Post)</small></a></li>
+                    <li><a href="#getPost">GET <small>(Fetch single Post)</small></a></li>
+                    <li><a href="#putPost">PUT <small>(Update a Post)</small></a></li>
+                    <li><a href="#deletePost">DELETE <small>(Delete a Post)</small></a></li>
+                </ul>
             </li>
         </ul>
         <hr>
         <!-- Category -->
         @includeIf('includes.blog.category')
+
+        <!-- Blog -->
+        @includeIf('includes.blog.post')
     </div>
 @stop
