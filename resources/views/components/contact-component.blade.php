9b474fa13c734d7c1ea544cc256f79a7e9dadf57
AR Shahin <mdshahinmije96@gmail.com>
2021-09-07 18:58:29 +0600

    contact form

--- resources/views/components/contact-component.blade.php
+++ resources/views/components/contact-component.blade.php
@@ -1,5 +1,5 @@
 <div>
     <div id="contact">
-       <a href="" title="Contact Us"> <i class="fab fa-facebook-messenger"></i></a>
+       <a href="{{ route('web.contact') }}" title="Contact Us"> <i class="fab fa-facebook-messenger"></i></a>
     </div>
 </div>
--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -101,7 +101,11 @@
         </div>
 <!-- Scroll Top -->
 <x-scroll-top/>
+
+@if (url()->current() !== route('web.contact'))
 <x-contact-component/>
+@endif
+
 <!-- Bootstrap Bundle with Popper -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
