577cd37a7f5f68cb4cb66e1070a5f10ceae2dced
AR Shahin <mdshahinmije96@gmail.com>
2021-09-08 13:45:53 +0600

    alert color change

--- resources/views/components/reactjs-example.blade.php
+++ resources/views/components/reactjs-example.blade.php
@@ -1,3 +1,3 @@
-<div class="alert alert-danger">
-    <p>If you want to see our API Example App and code with ReactJS, Please visit our <a href="https://abdulmajid.me/rte">ReactApp Example</a> and <a href="https://github.com/abdulmajidcse/reactjs-talktoapi-example">Github Repository</a>.</p>
+<div class="alert alert-success">
+    <p class="mb-0 pb-0">If you want to see our API Example App and code with ReactJS, Please visit our <a href="https://abdulmajid.me/rte" target="_blank">ReactApp Example</a> and <a href="https://github.com/abdulmajidcse/reactjs-talktoapi-example" target="_blank">Github Repository</a>.</p>
 </div>
